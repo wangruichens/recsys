75048e4aa69f200b8df2a92e4df62ad2a28c8e21
wangruichens <wangruichens@gmail.com>
2019-06-20 17:30:01 +0800

    update script

--- deepfm/gen_tfrecords.py
+++ deepfm/gen_tfrecords.py
@@ -4,7 +4,7 @@ spark = SparkSession.builder \
     .appName("deepfm_tfrecords") \
     .enableHiveSupport() \
     .getOrCreate()
-all_df = spark.sql("select * from mlg.tangjtest_20190515_article_rank_sample_2_tmp")
+all_df = spark.sql("select * from mlg.tablename")
 all_df.repartition(100).write.format("tfrecords")\
     .mode("overwrite").option("recordType", "Example")\
     .save('./deepfm_data/')
